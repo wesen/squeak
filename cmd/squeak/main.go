d62916d5798d4b6fcdcbe4692935e015150b13f4
Manuel Odendahl <wesen@ruinwesen.com>
2022-07-06 13:21:36 -0400

    :wrench: :sparkles: Properly override the config file in context with flags

--- cmd/squeak/main.go
+++ cmd/squeak/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"github.com/mattn/go-isatty"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/thediveo/enumflag"
+	"os"
 	"squeak/cmd/squeak/generate"
 	"squeak/lib"
+	"time"
 )
 import "github.com/rs/zerolog"
 
@@ -18,6 +22,26 @@ var rootCmd = cobra.Command{
 		if debug {
 			zerolog.SetGlobalLevel(zerolog.DebugLevel)
 		}
+		if isatty.IsTerminal(os.Stderr.Fd()) {
+			log.Debug().Msg("stderr is a terminal")
+			log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+		} else {
+			log.Debug().Msg("stderr is not a terminal")
+			log.Logger = log.Output(os.Stderr)
+		}
+
+		ctx := cmd.Context()
+		if ctx == nil {
+			log.Fatal().Msg("No context")
+		}
+		config, cast := ctx.Value("config").(*lib.Config)
+		if !cast {
+			log.Fatal().Msg("No config set in context")
+		}
+
+		if cmd.Flags().Lookup("dialect") != nil {
+			config.Dialect = overrideSQLDialect
+		}
 	},
 }
 
@@ -26,6 +50,7 @@ var SQLDialectIds = map[lib.SQLDialect][]string{
 	lib.MySQL:      {"mysql"},
 	lib.PostGreSQL: {"postgresql"},
 }
+var overrideSQLDialect lib.SQLDialect = lib.SQLite
 
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
@@ -34,14 +59,9 @@ func main() {
 	rootCmd.PersistentFlags().String("config", "", "Path to config file")
 	_ = rootCmd.MarkFlagRequired("config")
 
-	config := lib.NewConfig()
-
-	// we accept overrides for the different settings under `generate`
-	// in the YAML file.
-	// TODO(manuel): The problem here is that we will override the config settings if we pass
-	// a pointer to the variable directly
+	// we accept overrides for the different settings in the YAML file
 	rootCmd.PersistentFlags().VarP(
-		enumflag.New(&config.Dialect, "dialect",
+		enumflag.New(&overrideSQLDialect, "dialect",
 			SQLDialectIds, enumflag.EnumCaseInsensitive),
 		"dialect", "d",
 		"Dialect to use for the generated SQL statements")
@@ -49,6 +69,5 @@ func main() {
 	rootCmd.AddCommand(&generate.GenerateCmd)
 
 	ctx := context.WithValue(context.TODO(), "config", lib.NewConfig())
-
 	_ = rootCmd.ExecuteContext(ctx)
 }
